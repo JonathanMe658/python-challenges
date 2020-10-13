bd7dc42f8fe2dfc243a1bb78bbac3ffb32bdc1d1
Lucas Kustermans <LucasKustermans@Lucass-MacBook-Pro.local>
2020-10-13 15:08:54 +0200

    Turn_left and Turn_right methods added

--- python-robot-simulator/robot_simulator.py
+++ python-robot-simulator/robot_simulator.py
@@ -9,7 +9,6 @@ class Robot:
         self.bearing = bearing
         self.coordinates = (x, y)
 
-
     def advance(self):
         if self.bearing == NORTH:
             y = self.coordinates[1] + 1
@@ -24,12 +23,41 @@ class Robot:
             x = self.coordinates[0] - 1
             self.coordinates = (x, self.coordinates[1])
 
-
     def turn_left(self):
-        pass
+				if self.bearing == NORTH:
+						self.bearing == WEST
+						x = self.coordinates[0] - 1
+						self.coordinates = (x, self.coordinates[1])
+				elif self.bearing == WEST:	
+						self.bearing == SOUTH
+						y = self.coordinates[1] - 1
+						self.coordinates = (self.coordinates[0], y)
+				elif self.bearing == SOUTH:
+						self.bearing == EAST
+						x = self.coordinates[0] + 1
+						self.coordinates = (x, self.coordinates[1])
+				elif self.bearing == EAST:	                    			
+      			self.bearing == NORTH
+      			y = self.coordinates[1] - 1
+      			self.coordinates = (self.coordinates[0], y)
 
     def turn_right(self):
-        pass
+				if self.bearing == NORTH:                       			
+        		self.bearing == EAST
+        		x = self.coordinates[0] + 1
+        		self.coordinates = (x, self.coordinates[1])
+        elif self.bearing == EAST:	
+        		self.bearing == SOUTH
+        		y = self.coordinates[1] - 1
+        		self.coordinates = (self.coordinates[0], y)
+        elif self.bearing == SOUTH:
+        		self.bearing == WEST
+        		x = self.coordinates[0] - 1
+        		self.coordinates = (x, self.coordinates[1])
+        elif self.bearing == WEST:	                    
+        		self.bearing == NORTH
+        		y = self.coordinates[1] + 1
+	      		self.coordinates = (self.coordinates[0], y)
 
     def simulate(self):
         pass
